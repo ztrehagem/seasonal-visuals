1e84419744182b2d615e7810f1fcf59fcb584585
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2026-07-26 22:24:25 +0900

    commit

---
+++ src/client/java/dev/ztrehagem/seasonalvisuals/client/SeasonalVisualsClient.java
@@ -0,0 +1,10 @@
+package dev.ztrehagem.seasonalvisuals.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SeasonalVisualsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
